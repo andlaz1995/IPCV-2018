d865c14dfcb1dd0c69a2854a9a540f771277d624
jasonchanhh <jc15502@my.bristol.ac.uk>
2018-11-30 01:33:35 +0000

    added convolution and sobel operator

--- hough.cpp
+++ hough.cpp
@@ -15,35 +15,137 @@ using namespace cv;
 
 String cascade_name = "./dartcascade/cascade.xml";
 CascadeClassifier cascade;
-void detectAndDisplay( Mat frame );
-  Mat frame_gray;
-  Mat src_gray2;
 
 
+void convertGrey(Mat src, Mat src_gray) {
+  // Prepare Image by turning it into Grayscale and normalising lighting
+  cvtColor( src, src_gray, CV_BGR2GRAY );
+  equalizeHist(src_gray, src_gray);
+}
 
-void detectAndDisplay( Mat src ){
-  std::vector<Rect> dartboards;
+void convolve (Mat input, Mat kernel, Mat output) {
+    output.create(input.size(), input.type());
+
+    // pad input to prevent border effects
+    double unscaledOutput[input.rows][input.cols];
+    int kernelRadiusX = ( kernel.size[0] - 1 ) / 2;
+    int kernelRadiusY = ( kernel.size[1] - 1 ) / 2;
+
+    cv::Mat paddedInput;
+    cv::copyMakeBorder(input, paddedInput, kernelRadiusX, kernelRadiusX, kernelRadiusY, kernelRadiusY, cv::BORDER_REPLICATE);
+
+    // now we can do the convolution
+    for ( int i = 0; i < input.rows; i++ ) {
+        for( int j = 0; j < input.cols; j++ ) {
+            double sum = 0.0;
+
+            for( int m = -kernelRadiusX; m <= kernelRadiusX; m++ ) {
+                for( int n = -kernelRadiusY; n <= kernelRadiusY; n++ ) {
+                    // find the correct indices we are using
+                    int imagex = i + m + kernelRadiusX;
+                    int imagey = j + n + kernelRadiusY;
+                    int kernelx = m + kernelRadiusX;
+                    int kernely = n + kernelRadiusY;
+
+                    // get the values from the padded image and the kernel
+                    int imageval = ( int ) paddedInput.at<uchar>( imagex, imagey );
+                    double kernalval = kernel.at<double>( kernelx, kernely );
+
+                    // do the multiplication
+                    sum += imageval * kernalval;
+                }
+            }
+            // set the output value as the sum of the convolution
+            unscaledOutput[i][j] = sum;
+        }
+    }
+
+    // scale the convolution by a ratio of max-min
+    double min = 999999;
+    double max = 0;
+    for ( int i = 0; i < grey.rows; i++ ) {
+        for( int j = 0; j < grey.cols; j++ ) {
+            if(unscaledOutput[i][j] > tempMax) {
+                max = unscaledOutput[i][j];
+            }
+            if(unscaledOutput[i][j] < tempMin) {
+                min = unscaledOutput[i][j];
+            }
+        }
+    }
+
+    for ( int i = 0; i < grey.rows; i++ ) {
+        for( int j = 0; j < grey.cols; j++ ) {
+            output.at<uchar>(i, j) = (255)/(max-min)*(unscaledOutput[i][j]);
+        }
+    }
+}
+
+
+// TODO
+void sobelEdges(Mat input) {
+    cv::Mat gradientX, gradientY;
+    gradientX.create(input.size(), input.type());
+    gradientY.create(input.size(), input.type());
+
+    // values are flipped 180 because the convolve function is actually correlation??
+    float xvalues[] = {1,0,-1,2,0,-2,1,0,-1};
+    cv::Mat kernelX(3,3, CV_32F, dataX);
+
+    float yvalues[] = {1,2,1,0,0,0,-1,-2,-1};
+    cv::Mat kernelY(3,3, CV_32F, dataY);
+
+    //find dx and dy
+    convolve(input, kernelX, gradientX);
+    convolve(input, kernelY, gradientY);
+
+    return;
+}
+
+// TODO
+void houghCircles() {
+
+  return;
+}
+
+void violaJonesDetector(Mat src, vector<Rect> viola_dartboards) {
+  // Perform Viola-Jones Object Detection
+  cascade.detectMultiScale( src, viola_dartboards, 1.1, 1, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50), Size(500,500) );
+
+  // Draw box around faces found
+  // for( int i = 0; i < viola_dartboards.size(); i++ ) {
+  //   rectangle(out, Point(viola_dartboards[i].x, viola_dartboards[i].y), Point(viola_dartboards[i].x + viola_dartboards[i].width, viola_dartboards[i].y + viola_dartboards[i].height), Scalar( 0, 255, 0 ), 2);
+  // }
+  return;
+}
+
+// TODO
+void combineDetections() {
+  return;
+}
+
+int main( int argc, const char** argv )
+{
+   // Read Input Image
+	Mat frame = imread(argv[1], CV_LOAD_IMAGE_COLOR);
   Mat src_gray;
+  std::vector<Rect> viola_dartboards;
 
-  // 1. Prepare Image by turning it into Grayscale and normalising lighting
-  cvtColor( src, src_gray2, CV_BGR2GRAY );
-  equalizeHist( src_gray2, src_gray2 );
+  // Load the Strong Classifier in a structure called `Cascade'
+  if( !cascade.load( cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
 
-  // 2. Perform Viola-Jones Object Detection
-  cascade.detectMultiScale( src_gray2, dartboards, 1.1, 1, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50), Size(500,500) );
-  // QUESTION: do we need to adjust any of these params?
+  convertGrey(frame, src_gray);
 
-       // 3. Print number of Faces found
-  std::cout << dartboards.size() << std::endl;
+  sobelEdges();
 
-       // 4. Draw box around faces found
-  for( int i = 0; i < dartboards.size(); i++ )
-  {
+  houghCircles();
 
-    //rectangle(src, Point(dartboards[i].x, dartboards[i].y), Point(dartboards[i].x + dartboards[i].width, dartboards[i].y + dartboards[i].height), Scalar( 0, 255, 0 ), 2);
-    // std::cout << dartboards[i].x << std::endl; // prints bounding box locations
-    // std::cout << dartboards[i].y << std::endl;
-    // printf("..");
+	violaJonesDetector(frame, viola_dartboards);
 
-  }
+  combineDetections();
+
+	// 4. Save Result Image
+	imwrite( "detected.jpg", frame );
+
+	return 0;
 }
